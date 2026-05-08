b994a375e9db433b2ac54afb7dcc34c3aa8a31d1
Martin Losinský <52763552+mlosinsky@users.noreply.github.com>
2026-05-08 20:16:20 +0200

    Invisible debug log

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 export default {
   fetch(request: Request): Response {
+    console.debug("this log isn't visible");
     console.log("In function, hello world!");
     console.log("URL="+request.url);
     console.log("EXAMPLE_ENV_VAR set", Deno.env.has("EXAMPLE_ENV_VAR"));
