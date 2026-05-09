03ddde00e7eecda20152f41dbcfb9fbc883b6dd6
Martin Losinský <52763552+mlosinsky@users.noreply.github.com>
2026-05-09 15:43:28 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 export default {
   fetch(request: Request): Response {
     console.debug("this log isn't visible");
-    console.log("In function, hello world!");
+    console.log("In function, hello world 123!");
     console.log("URL="+request.url);
     console.log("EXAMPLE_ENV_VAR set", Deno.env.has("EXAMPLE_ENV_VAR"));
     console.log("EXAMPLE_ENV_VAR=", Deno.env.get("EXAMPLE_ENV_VAR"));
